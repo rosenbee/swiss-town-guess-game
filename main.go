99f90dcc6ee9a17890764869119a7e44cf9e0aba
carli beeli BIT <carli.beeli@bit.admin.ch>
2022-08-09 13:45:44 +0200

    opening screen

--- main.go
+++ main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"golang.org/x/crypto/ssh/terminal"
 	towndata "swiss-town-guess-game.com/swiss-town-guess-game/swisspostdata"
 )
 
 func main() {
-	fmt.Println("/// just testing")
+	fmt.Println("**************************************************************************************************************")
+	fmt.Println("* Welcome to the Swiss Town Guess Game!                                                                      *")
+	fmt.Println("*                                                                                                            *")
+	fmt.Println("* Thanks to post.ch / swisspost.opendatasoft.com / BFS for providing free data for this quiz.                *")
+	fmt.Println("**************************************************************************************************************")
+
+	fmt.Printf("Please enter your valid swisspost.opendatasoft.com API-Key: ")
+	apiKey, err := terminal.ReadPassword(0)
+	if err != nil {
+		fmt.Println("Could not read api key")
+		os.Exit(1)
+	}
+	fmt.Println()
+
+	fmt.Println("Key entered: ")
+	fmt.Println(string(apiKey))
 
 	fmt.Println(towndata.HelloTest)
 }
